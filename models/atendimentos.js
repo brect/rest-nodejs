f0faee961ca92cad0f6b6bad0a9529e9a4ca4966
brect <b.limas93@gmail.com>
2019-12-13 20:27:08 -0300

    Projeto petshop seguindo padrao Rest com NodeJS, criando API com Express e MySQL

---
+++ models/atendimentos.js
@@ -0,0 +1,102 @@
+const moment = require('moment')
+const conexao = require('../infraestrutura/conexao')
+
+class Atendimento {
+
+    adiciona(atendimento, res) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+        
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+        const clienteEhValido = atendimento.cliente.length >= 5;
+
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
+            }
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido);
+        const existemErros = erros.length;
+
+        if(existemErros) {
+            res.status(400).json(erros);
+        } else {
+            const atendimentoDatado = {...atendimento, dataCriacao, data};
+
+            const sql = 'INSERT INTO Atendimentos SET ?';
+    
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro) {
+                    res.status(400).json(erro);
+                } else {
+                    res.status(201).json(atendimento);
+                }
+            })
+        }
+       
+    }
+
+    lista(res){
+        const sql = 'SELECT * FROM atendimentos';
+
+        conexao.query(sql, (erro, resultados) => {
+            if (erro) {
+                res.status(400).json(erro);
+            } else {
+                res.status(200).json(resultados);
+            }
+        })
+    }
+
+    buscaPorId(id, res){
+        const sql = `SELECT * FROM ATENDIMENTOS WHERE ID=${id}`;
+
+        conexao.query(sql, (erro, resultados)=>{
+            const atendimento = resultados[0];
+            if (erro) {
+                res.status(400).json(erro);
+            } else {
+                res.status(200).json(atendimento);
+            }
+        })
+    }
+
+    altera(id, valores, res){
+
+        if (valores.data) {
+            valores.data = moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+        }
+
+        const sql = 'UPDATE ATENDIMENTOS SET ? WHERE ID=?';
+
+        conexao.query(sql, [valores, id], (erro, resultados)=>{
+            if (erro) {
+                res.status(400).json(erro);
+            } else {
+                res.status(200).json(resultados);
+            }
+        });
+    }
+
+    deleta(id, res){
+        const sql = 'DELETE FROM ATENDIMENTOS WHERE ID=?';
+
+        conexao.query(sql, id, (erro, res)=>{
+            if (erro) {
+                res.status(400).json(erro);
+            } else {
+                res.status(200).json(atendimento);
+            }
+        });
+    }
+}
+
+module.exports = new Atendimento
